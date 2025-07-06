2f2414a3b2de7c6ecbc156d7997e2da2c33c5e88
Chloë Rice <chloericetong@gmail.com>
2025-07-06 10:34:22 -0700

    added api key to .env

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ## To run
 
-1. `npm i`
-2. `npm run dev`
+1. make a copy of `.env.example` named `.env`
+2. put in an api key from [Exchange Rate API](https://app.exchangerate-api.com/)
+3. `npm i`
+4. `npm run dev`
--- src/view/CurrencyConverter.tsx
+++ src/view/CurrencyConverter.tsx
@@ -3,7 +3,9 @@ import { useListCountries } from "../api/hooks/useListCountries.ts";
 export const CurrencyConverter = () => {
   const { data } = useListCountries();
 
-  console.log({ data });
+  const apiKey = import.meta.env.VITE_EXCHANGE_RATE_API_KEY;
+
+  console.log({ data, apiKey });
 
   return <div>currency converter</div>;
 };
